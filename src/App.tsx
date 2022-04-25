6c8d9e090264136670a02e7b7442386dfd0d0fe8
ELLIOTTCABLE <git@ell.io>
2022-04-25 18:40:56 -0500

    (new dep) Add a Monaco editor instance

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,27 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Editor from './Editor'
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => {
-            // debugger;
-            setCount((count) => count + 1)
-          }
-          }>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+function SelectFileButton() {
+   return <button>Select ModsConfig</button>
+}
+
+function Header() {
+   return (
+      <>
+         <SelectFileButton />
+      </>
+   )
+}
+
+function App() {
+   return (
+      <>
+         <Header />
+         <Editor />
+      </>
+   )
 }
 
 export default App
---
+++ src/Editor.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import MonacoEditor from '@monaco-editor/react'
+import draculaTheme from './Dracula.monacotheme.json'
+import type { Monaco } from 'monaco-editor'
+
+export default function Editor() {
+   function handleEditorWillMount(monaco: Monaco) {
+      monaco.editor.defineTheme('dracula', draculaTheme)
+   }
+
+   return (
+      <MonacoEditor
+         className="editor"
+         height="90vh"
+         theme="dracula"
+         defaultLanguage="xml"
+         defaultValue="<!-- Click above to begin -->"
+         beforeMount={handleEditorWillMount}
+      />
+   )
+}
