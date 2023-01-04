baf6a9645e6f38089a51f0d86c19ac0bce616973
ELLIOTTCABLE <git@ell.io>
2023-01-04 17:39:30 -0600

    (- fix des) Reset Monaco's typeface jankily

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -12,6 +12,22 @@ export interface Props {
    fileIsSelected?: boolean
 }
 
+// TODO: Derive this dynamically, somehow, from
+//    <https://github.com/csstools/sanitize.css/blob/main/typography.css>
+const uiMonospace = `ui-monospace,
+   /* macOS 10.10+ */ "Menlo",
+   /* Windows 6+ */ "Consolas",
+   /* Android 4+ */ "Roboto Mono",
+   /* Ubuntu 10.10+ */ "Ubuntu Monospace",
+   /* KDE Plasma 5+ */ "Noto Mono",
+   /* KDE Plasma 4+ */ "Oxygen Mono",
+   /* Linux/OpenOffice fallback */ "Liberation Mono",
+   /* fallback */ monospace,
+   /* macOS emoji */ "Apple Color Emoji",
+   /* Windows emoji */ "Segoe UI Emoji",
+   /* Windows emoji */ "Segoe UI Symbol",
+   /* Linux emoji */ "Noto Color Emoji"`
+
 export default function Editor({ modsConfigFile, fileIsSelected = false }: Props) {
    const [content, setContent] = useState("<!-- Uninitialized content -->")
    const editorRef = useRef<MonacoT.editor.IStandaloneCodeEditor | null>(null)
@@ -59,6 +75,11 @@ export default function Editor({ modsConfigFile, fileIsSelected = false }: Props
          options={{
             formatOnPaste: true,
             formatOnType: true,
+
+            // TODO: Derive these dynamically, somehow
+            fontFamily: uiMonospace,
+            fontSize: 16,
+            lineHeight: 1.375, // Dracula --line-height-md
          }}
          className="editor"
          height="100%"
